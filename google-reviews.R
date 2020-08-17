d5889d7ee9d24b341e10b288c3a2aa455b5db6d9
AmandaRP <amanda@dekleineorchards.com>
2020-08-17 14:50:21 +0000

    Added python code to read google reviews data.

--- google-reviews.R
+++
@@ -1,7 +0,0 @@
-
-
-user_data <- jsonlite::stream_in(gzcon(url("http://deepyeti.ucsd.edu/jmcauley/datasets/googlelocal/users.clean.json.gz")))
-
-tmp <- tempfile()
-download.file("http://deepyeti.ucsd.edu/jmcauley/datasets/googlelocal/users.clean.json.gz", tmp)
-user_data <- jsonlite::stream_in(gzfile(tmp))
